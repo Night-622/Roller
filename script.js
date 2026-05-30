c4c749c55a21b07fcd083637604d2f58f60342e7
Night <george@katris.net>
2026-05-30 21:22:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var BOOST_DRAIN_TIME = 4000;
 var BRAKE_POWER = 0.97;
 var BRAKE_REVERSE = 0.0009;
 
+
 var CLUTCH_FRICTION = 0.9999; // Decel rate while clutch held (lower = stops faster, higher = coasts longer; between BRAKE_POWER and 0.99)
 
 // Count number of set bits in a bitmask (used for checkpoint progress)
